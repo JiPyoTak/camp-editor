3a0a717ede443c0ebeb02b1b7a08203cc18116b0
seokho10007 <sukho1007@naver.com>
2023-01-31 17:34:52 +0900

    feat: class Editor 생성 (#1)
    Co-authored-by: jintak0401 <jintak0401@naver.com>
    Co-authored-by: pyo-sh <pygosky@gmail.com>

---
+++ src/editor.ts
@@ -0,0 +1,37 @@
+import { EditorView } from '@/core/EditorView';
+import { getOptions } from '@/utils/option';
+import type { CampEditorOptions } from '@t/.';
+
+class Editor {
+  $root: HTMLElement;
+  options: CampEditorOptions;
+  _editorView: EditorView;
+
+  constructor(
+    idOrEl: HTMLElement | string,
+    options: Partial<CampEditorOptions> = {},
+  ) {
+    this.$root = this.getRootElement(idOrEl);
+    this.options = getOptions(options);
+    this._editorView = new EditorView(this.$root, this.options);
+  }
+
+  getRootElement(idOrEl: HTMLElement | string) {
+    let $element = null;
+
+    if (idOrEl instanceof HTMLElement) {
+      $element = idOrEl;
+    } else if (typeof idOrEl === 'string') {
+      $element = document.getElementById(idOrEl);
+    } else {
+      throw new Error('초기화 에러 발생');
+    }
+
+    if (!$element) {
+      throw new Error('태그가 존재하지 않습니다.');
+    }
+    return $element;
+  }
+}
+
+export { Editor };
