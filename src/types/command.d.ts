2c35b47fd22c1b9a0c26bd861932bd31e4eddf96
seokho10007 <sukho1007@naver.com>
2023-01-31 17:23:14 +0900

    feat: Editor 관련 type 추가 (#1)
    Co-authored-by: jintak0401 <jintak0401@naver.com>
    Co-authored-by: pyo-sh <pygosky@gmail.com>

---
+++ src/types/command.d.ts
@@ -0,0 +1,42 @@
+type CampCommand =
+  | 'undo'
+  | 'redo'
+  | 'font'
+  | 'font-size'
+  | 'bold'
+  | 'underline'
+  | 'italic'
+  | 'strike'
+  | 'subscript'
+  | 'superscript'
+  | 'remove-format'
+  | 'heading'
+  | 'font-color'
+  | 'font-background-color'
+  | 'code-block'
+  | 'inline-code-block'
+  | 'indent'
+  | 'outdent'
+  | 'order-list'
+  | 'unorder-list'
+  | 'align'
+  | 'horizontal-line'
+  | 'line-height'
+  | 'table'
+  | 'image'
+  | 'video'
+  | 'preview'
+  | 'show-block'
+  | 'full-screen'
+  | 'help';
+
+interface CampCommandDetail {
+  icon: string;
+  tagName?: keyof HTMLElementTagNameMap;
+}
+
+type CampCommandInfo = {
+  [key in CampCommand]: CampCommandDetail;
+};
+
+export type { CampCommand, CampCommandDetail, CampCommandInfo };
