73fcf4dd5f30c9bf8dbb4e465123f66e2238ee2b
seokho10007 <sukho1007@naver.com>
2023-01-31 17:21:22 +0900

    feat: command 관련 constants 설정#1
    Co-authored-by: jintak0401 <jintak0401@naver.com>
    Co-authored-by: pyo-sh <pygosky@gmail.com>

---
+++ src/constants/command.ts
@@ -0,0 +1,22 @@
+import type { CampCommandInfo } from '@t/.';
+
+const COMMAND_INFO: CampCommandInfo = Object.freeze({
+  bold: {
+    tagName: 'strong',
+    icon: 'https://img.icons8.com/fluency-systems-filled/48/000000/bold.png',
+  },
+  italic: {
+    tagName: 'i',
+    icon: 'https://img.icons8.com/fluency-systems-filled/48/000000/italic.png',
+  },
+  underline: {
+    tagName: 'u',
+    icon: 'https://img.icons8.com/fluency-systems-filled/48/000000/underline.png',
+  },
+  strike: {
+    tagName: 'del',
+    icon: 'https://img.icons8.com/fluency-systems-filled/30/000000/strikethrough.png',
+  },
+}) as CampCommandInfo;
+
+export { COMMAND_INFO };
