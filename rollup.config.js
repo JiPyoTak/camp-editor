0f61976350aad1de35abb6a5a87b1c504962c200
seokho10007 <sukho1007@naver.com>
2023-01-31 17:20:19 +0900

    chore: types 폴더 절대경로 설정
    Co-authored-by: jintak0401 <jintak0401@naver.com>
    Co-authored-by: pyo-sh <pygosky@gmail.com>

--- rollup.config.js
+++ rollup.config.js
@@ -1,42 +1,46 @@
-import path from "path";
-import del from "rollup-plugin-delete";
-import typescript from "rollup-plugin-typescript2";
-import dts from "rollup-plugin-dts";
-import prettier from "rollup-plugin-prettier";
-import alias from "@rollup/plugin-alias";
+import path from 'path';
+import del from 'rollup-plugin-delete';
+import typescript from 'rollup-plugin-typescript2';
+import dts from 'rollup-plugin-dts';
+import prettier from 'rollup-plugin-prettier';
+import alias from '@rollup/plugin-alias';
 
 const paths = [
   {
-    find: "@",
-    replacement: path.resolve(__dirname, "src"),
+    find: '@',
+    replacement: path.resolve(__dirname, 'src'),
+  },
+  {
+    find: '@t',
+    replacement: path.resolve(__dirname, 'src/types'),
   },
 ];
 
 export default [
   {
-    input: "src/index.ts",
+    input: 'src/index.ts',
     output: {
-      dir: "dist",
-      format: "esm",
+      dir: 'dist',
+      format: 'esm',
     },
     plugins: [
       typescript(),
-      del({ targets: "dist/*" }),
+      del({ targets: 'dist/*' }),
       alias({
         entries: paths,
       }),
     ],
   },
   {
-    input: "src/index.ts",
-    output: [{ file: "dist/index.d.ts", format: "cjs" }],
+    input: 'src/index.ts',
+    output: [{ file: 'dist/index.d.ts', format: 'cjs' }],
     plugins: [
       dts.default(),
       alias({
         entries: paths,
       }),
       prettier({
-        parser: "babel",
+        parser: 'babel',
         tabWidth: 2,
       }),
     ],
