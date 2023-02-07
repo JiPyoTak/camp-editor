0584a39658714ef6806a0186d588f456290f844c
pyo-sh <pygosky@gmail.com>
2023-02-07 15:04:09 +0900

    refactor: 드래그 후 Font-Format을 위한 라인 분리에서 공통 로직 함수화 (#3)

--- src/utils/class/Lines.ts
+++ src/utils/class/Lines.ts
@@ -21,6 +21,14 @@ class Lines implements ILinesInfo {
     this.$nodes = $lines.map(($line) => this.copyLine($line));
   }
 
+  setUnderRoot(attributeName: string, $target: Node | null) {
+    if (attributeName === '$from') {
+      this.$from = $target;
+    } else if (attributeName === '$to') {
+      this.$to = $target;
+    }
+  }
+
   copyLine($line: Node) {
     const $copiedRoot = $line.cloneNode(false);
 
@@ -37,150 +45,89 @@ class Lines implements ILinesInfo {
     const isStart = $current === startContainer;
     const isEnd = $current === endContainer;
 
-    // 복사하는 노드가 startContainer or endContainer 일 때
-    // TODO : startContainer와 endContainer가 p 태그일 때 작동
-    // TODO : 중복 코드 리팩토링하기
-    if (isStart) {
+    if (isStart || isEnd) {
+      const offset = isStart ? startOffset : endOffset;
+      let $copiedLeft: Node | null = null;
+      let $copiedRight: Node | null = null;
+      let leftChildNodes: Node[] = [];
+      let rightChildNodes: Node[] = [];
+
       if ($current.nodeType === Node.TEXT_NODE) {
-        let [$copiedLeft, $copiedRight] = splitTextNode($current, startOffset);
-
-        const { $nowUnderRoot } = this.saveContainers($copiedLeft, parentLink);
-        this.$from = $nowUnderRoot ?? $copiedRight;
-        console.log($copiedRight, $nowUnderRoot);
-
-        if ($copiedRight) {
-          const $currentParent = parentLink.getValue();
-          $currentParent.appendChild($copiedRight);
-        }
+        [$copiedLeft, $copiedRight] = splitTextNode($current, offset);
       } else {
-        const { childNodes: $childNodes } = startContainer;
-        let $copiedLeft = startOffset === 0 ? null : $current.cloneNode(false);
-        const leftChildNodes = Array.prototype.slice.call(
-          $childNodes,
-          0,
-          startOffset,
-        );
-        let $copiedRight =
-          startOffset === $childNodes.length ? null : $current.cloneNode(false);
-        const rightChildNodes = Array.prototype.slice.call(
-          $childNodes,
-          startOffset,
-        );
+        const { childNodes: $childNodes } = $current;
 
-        const { $nowUnderRoot } = this.saveContainers(
-          $copiedLeft,
-          parentLink,
-          leftChildNodes,
-        );
-        this.$from = $nowUnderRoot ?? $copiedRight;
+        $copiedLeft = offset === 0 ? null : $current.cloneNode(false);
+        leftChildNodes = Array.prototype.slice.call($childNodes, 0, offset);
 
-        if ($copiedRight) {
-          const currentLink = new LinkedList($copiedRight as Node, parentLink);
-          const $currentParent = parentLink.getValue();
-          $currentParent.appendChild($copiedRight);
-          rightChildNodes.forEach((node) => {
-            this.copyDFS(node, currentLink);
-          });
-        }
+        $copiedRight =
+          offset === $childNodes.length ? null : $current.cloneNode(false);
+        rightChildNodes = Array.prototype.slice.call($childNodes, offset);
       }
-    } else if (isEnd) {
-      if ($current.nodeType === Node.TEXT_NODE) {
-        let [$copiedLeft, $copiedRight] = splitTextNode($current, endOffset);
 
-        const { $prevUnderRoot } = this.saveContainers($copiedLeft, parentLink);
-        this.$to = $prevUnderRoot ?? $copiedLeft;
+      if ($copiedLeft) {
+        this.appendAndCopyChildren($copiedLeft, leftChildNodes, parentLink);
+      }
 
-        if ($copiedRight) {
-          const $currentParent = parentLink.getValue();
-          $currentParent.appendChild($copiedRight);
-        }
-      } else {
-        const { childNodes: $childNodes } = endContainer;
-        let $copiedLeft = endOffset === 0 ? null : $current.cloneNode(false);
-        const leftChildNodes = Array.prototype.slice.call(
-          $childNodes,
-          0,
-          endOffset,
-        );
-        let $copiedRight =
-          endOffset === $childNodes.length ? null : $current.cloneNode(false);
-        const rightChildNodes = Array.prototype.slice.call(
-          $childNodes,
-          endOffset,
-        );
+      const { $nowUnderRoot, $prevUnderRoot } = this.saveAsOther(
+        $copiedRight,
+        parentLink,
+      );
+      const saveAttribute = isStart ? '$from' : '$to';
+      const $saveUnderRoot = isStart
+        ? $nowUnderRoot ?? $copiedRight
+        : $prevUnderRoot ?? $copiedLeft;
+      this.setUnderRoot(saveAttribute, $saveUnderRoot);
 
-        const { $prevUnderRoot } = this.saveContainers(
-          $copiedLeft,
-          parentLink,
-          leftChildNodes,
-        );
-        this.$to = $prevUnderRoot ?? $copiedLeft;
-
-        if ($copiedRight) {
-          const currentLink = new LinkedList($copiedRight as Node, parentLink);
-          const $currentParent = parentLink.getValue();
-          $currentParent.appendChild($copiedRight);
-          rightChildNodes.forEach((node) => {
-            this.copyDFS(node, currentLink);
-          });
-        }
+      if ($copiedRight) {
+        this.appendAndCopyChildren($copiedRight, rightChildNodes, parentLink);
       }
     } else {
       const $currentCopied: Node = $current.cloneNode(false);
-      const currentLink = new LinkedList($currentCopied as Node, parentLink);
-      const $currentParent = parentLink.getValue();
-      $currentParent.appendChild($currentCopied);
-
-      childNodes.forEach((node) => {
-        this.copyDFS(node, currentLink);
-      });
+      this.appendAndCopyChildren($currentCopied, childNodes, parentLink);
     }
   }
 
-  saveContainers(
-    $targetLeft: Node | null,
+  appendAndCopyChildren(
+    $target: Node,
+    childNodes: Node[] | NodeListOf<ChildNode>,
     parentLink: LinkedList<Node>,
-    $targetChilds: Node[] = [] as Node[],
   ) {
-    // 자기 자신 넣고
-    //// 자식들 넣고
-    if ($targetLeft) {
-      const $currentParent = parentLink.getValue();
-      $currentParent.appendChild($targetLeft);
-      const currentLink = new LinkedList($targetLeft as Node, parentLink);
-      $targetChilds.forEach(($child) => {
-        this.copyDFS($child, currentLink);
-      });
-    }
+    const currentLink = new LinkedList($target, parentLink);
+    const $currentParent = parentLink.getValue();
+    $currentParent.appendChild($target);
 
+    childNodes.forEach(($node) => {
+      this.copyDFS($node, currentLink);
+    });
+  }
+
+  saveAsOther($target: Node | null, parentLink: LinkedList<Node>) {
     let nowLink: LinkedList<Node> | null = parentLink;
-    let $prevNode: Node | null = null;
+    let $prevCopiedNode: Node | null = $target;
     let $nowUnderRoot: Node | null = null;
     let $prevUnderRoot: Node | null = null;
 
-    // 위로 복사하며 부모 참조 바꾸기
-    while (nowLink?.getPrev()) {
+    while (nowLink) {
+      const $child = $prevCopiedNode;
       const $currentParent = nowLink.getValue();
-      const $copiedParent = $currentParent.cloneNode(false);
 
-      if ($prevNode) {
-        $copiedParent.appendChild($prevNode);
+      if (!nowLink.isRoot()) {
+        $prevCopiedNode = $currentParent.cloneNode(false);
+        $prevUnderRoot = $currentParent;
+        nowLink.setValue($prevCopiedNode);
       }
 
-      nowLink.setValue($copiedParent);
+      const $parent = nowLink.getValue();
+      if ($child) {
+        $parent.appendChild($child);
+      }
+
       nowLink = nowLink.getPrev();
-      $nowUnderRoot = $copiedParent;
-      $prevUnderRoot = $currentParent;
-      $prevNode = $copiedParent;
+      $nowUnderRoot = $prevCopiedNode;
     }
 
-    // P 아래로 복사한 게 있다면 넣어주기
-    if ($nowUnderRoot) {
-      const $line = nowLink?.getValue();
-      $line?.appendChild($nowUnderRoot);
-    }
-
-    return { $prevUnderRoot, $nowUnderRoot };
+    return { $nowUnderRoot, $prevUnderRoot };
   }
 }
 
