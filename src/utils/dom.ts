d04ca5b623b0ec89712f29ac93ddc5673a0bad52
seokho10007 <sukho1007@naver.com>
2023-02-16 15:51:48 +0900

    feat: position 계산 유틸함수 추가 (#6)

--- src/utils/dom.ts
+++ src/utils/dom.ts
@@ -276,3 +276,22 @@ export function wrapLines(lineInfos: ICopiedLinesInfo[], tagName: string) {
 
   return { $lines, $startContainer, $endContainer };
 }
+
+export const getRelativePosition = (
+  $parent: HTMLElement,
+  $target: HTMLElement,
+) => {
+  const parentRect = $parent.getBoundingClientRect();
+  const targetRect = $target.getBoundingClientRect();
+
+  const elementTop = window.pageYOffset + parentRect.top;
+  const targetTop = window.pageYOffset + targetRect.top;
+
+  const elementLeft = window.pageXOffset + parentRect.left;
+  const targetLeft = window.pageXOffset + targetRect.left;
+
+  return {
+    left: Math.abs(targetLeft - elementLeft),
+    top: parentRect.height - Math.abs(elementTop - targetTop),
+  };
+};
